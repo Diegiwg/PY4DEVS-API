bcbbbfc0ed7311cdf8e79206f56fc69a91607954
Diegiwg <diegiwg@gmail.com>
2021-07-27 01:54:59 -0300

    TRABALHO EM PROGRESSO - V0.1.0 WIP

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup (
+    name='py4devs_api',
+    packages=find_packages(include=['py4devs_api']),
+    version='0.1.0',
+    description='...',
+    author='Diego Queiroz [Diegiwg]',
+    license='MIT'
+)
