b008160e6f4c9281379cd81abd1b2cc055980994
Diegiwg <diegiwg@gmail.com>
2021-07-28 10:41:46 -0300

    The library was rewritten, and some functions were deleted, as they were not in line with the project's initial proposal.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open('README.md', encoding='utf-8') as f: long_description = f.read()
 setup (
     name='py4devs_api',
     packages=find_packages(include=['py4devs_api']),
-    version='0.2.0',
-    description='A biblioteca py4devs tem por objetivo fornecer um meio simples de utilizar as incriveis ferramentas desenvolvidas pela equipe 4DEVS.COM.BR diretamente nos scripts/projetos python.',
+    version='0.2.1',
+    description='API4DEVS is a free library that makes it possible to generate Brazilian Documents to test verification functions in the most diverse applications.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Diego Queiroz [Diegiwg]',
     author_email='diegiwg@gmail.com',
     license='MIT',
     url='https://github.com/Diegiwg/PY4DEVS-API',
-    download_url = 'https://github.com/Diegiwg/PY4DEVS-API/archive/refs/tags/v0.2.0.tar.gz',
+    download_url = 'https://github.com/Diegiwg/PY4DEVS-API/archive/refs/tags/v0.2.1.tar.gz',
     install_requires=['requests==2.26.0','beautifulsoup4==4.9.3'],
     classifiers=[
 	    'Development Status :: 3 - Alpha',
